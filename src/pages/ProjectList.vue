18771c264dc28b21c5c935a77a2b5dfb14aa639d
michelecanini <studio.michelecanini@gmail.com>
2023-09-02 19:23:31 +0200

    [FIX] correct link syntax href vue

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -74,8 +74,8 @@ export default {
                         <h5 class="card-title text-black mt-3">{{ project.title }}</h5>
                         <div><span class="badge text-bg-warning me-1" v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</span></div>
                         <p class=" text-black mt-3">{{ truncateText(project.description) }}</p>      
-                        <a href="{{ project.github }}" target="_blank" class="btn btn-primary mt-3 me-2">GitHub</a>
-                        <a href="{{ project.demo }}" target="_blank" class="btn btn-secondary mt-3">Demo</a>
+                        <a :href="project.github" target="_blank" class="btn btn-primary mt-3 me-2">GitHub</a>
+                        <a :href="project.demo" target="_blank" class="btn btn-secondary mt-3">Demo</a>
                     </div>
                 </div>
             </div>
